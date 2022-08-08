80b3d366166f19f6e125b53694c5720ed2b52cae
hibakun <hiba.kudus@gmail.com>
2022-08-08 18:59:39 +0700

    [HF] menambahkan kondisi

--- lib/services/remote_service.dart
+++ lib/services/remote_service.dart
@@ -2,14 +2,19 @@ import 'package:http/http.dart';
 import '../models/siswa.dart';
 
 class RemoteService {
+  static const String url = "https://pplgrestapi.000webhostapp.com/api/siswa";
+
   Future<Siswa?> getData() async {
     var client = Client();
     
-    var uri = Uri.parse("https://pplgrestapi.000webhostapp.com/api/siswa");
+    var uri = Uri.parse(url);
     var response = await client.get(uri);
+
     if (response.statusCode == 200) {
       var json = response.body;
       return siswaFromJson(json);
+    } else {
+      throw Exception("Failed to fetch data from API");
     }
   }
 }
